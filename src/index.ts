6075fa2e73ad04459c705be1f12d365ef9ae8a13
Sebastian Seitz <bassistsagainstracists@gmx.net>
2019-07-01 10:13:11 +0200

    add abort controller support

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
+export interface FramusOptions {
+  signal?: AbortSignal
+}
+
+export type FramusRequestCallback<T> = (now: DOMHighResTimeStamp) => T
+
 /**
  * Resolves with the high res time stamp of the next animation frame
  */
-export const framus = () => new Promise<DOMHighResTimeStamp>(window.requestAnimationFrame)
+export const framus = <T = unknown>(
+  callback: FramusRequestCallback<T> | FramusOptions = {},
+  { signal }: FramusOptions = typeof callback === 'function' ? {} : callback
+): Promise<T | DOMHighResTimeStamp> => new Promise((resolve, reject) => {
+  const request = window.requestAnimationFrame(now => {
+    if (signal) {
+      signal.removeEventListener('abort', handleAbort)
+    }
+
+    resolve(typeof callback === 'function' ? callback(now) : now)
+  })
+
+  const handleAbort = () => {
+    window.cancelAnimationFrame(request)
+    reject(new DOMException('Cancelled animation frame request'))
+  }
+
+  if (signal) {
+    signal.addEventListener('abort', handleAbort)
+  }
+})
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,23 +1,68 @@
 import { framus } from '../src'
 
+const afterNextFrame = (callback: (now: DOMHighResTimeStamp) => void) =>
+  window.requestAnimationFrame(now => window.setTimeout(() => callback(now)))
+
 describe('framus', () => {
+  const callback = jest.fn()
   const handleError = jest.fn()
 
   afterEach(() => {
-    expect(handleError).not.toHaveBeenCalled()
+    callback.mockClear()
+    handleError.mockClear()
   })
 
   it('should request an animation frame', done => {
-    const fn = jest.fn()
+    framus().then(callback).catch(handleError)
 
-    framus().then(fn).catch(handleError)
+    afterNextFrame(now => {
+      const [[ arg ]] = callback.mock.calls
 
-    window.requestAnimationFrame((now) => window.setTimeout(() => {
-      const [[ arg ]] = fn.mock.calls
-
-      expect(fn).toHaveBeenCalled()
-      expect(arg).toBeLessThanOrEqual(now)
+      expect(callback).toHaveBeenCalled()
+      expect(arg).toBeLessThan(now)
+      expect(handleError).not.toHaveBeenCalled()
       done()
-    }))
+    })
+  })
+
+  it('should accept a callback', done => {
+    callback.mockImplementation(now => now * 2)
+
+    framus(callback).then(value => {
+      const [[ arg ]] = callback.mock.calls
+
+      expect(callback).toHaveBeenCalled()
+      expect(value).toBe(arg * 2)
+      done()
+    }).catch(handleError)
+  })
+
+  it('should cancel an animation frame', done => {
+    const controller = new AbortController()
+
+    framus({
+      signal: controller.signal
+    }).then(callback).catch(handleError)
+
+    controller.abort()
+
+    afterNextFrame(() => {
+      expect(callback).not.toHaveBeenCalled()
+      expect(handleError).toHaveBeenCalled()
+      done()
+    })
+  })
+
+  it('should accept a callback and an options object', done => {
+    const signal = new AbortController().signal
+    const addEventListenerSpy = jest.spyOn(signal, 'addEventListener')
+    const removeEventListenerSpy = jest.spyOn(signal, 'removeEventListener')
+
+    framus(() => {
+      expect(removeEventListenerSpy).toHaveBeenCalled()
+      done()
+    }, { signal }).catch(handleError)
+
+    expect(addEventListenerSpy).toHaveBeenCalled()
   })
 })
