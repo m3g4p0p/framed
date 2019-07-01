59221657ee26fb4a6c8d1e1e18fb49c355103366
Sebastian Seitz <bassistsagainstracists@gmx.net>
2019-07-01 10:18:59 +0200

    improve overload declaration

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,34 @@ export interface FramusOptions {
 }
 
 export type FramusRequestCallback<T> = (now: DOMHighResTimeStamp) => T
+export function framus (options?: FramusOptions): Promise<DOMHighResTimeStamp>
+export function framus <T> (callback: FramusRequestCallback<T>, options?: FramusOptions): Promise<T>
 
 /**
  * Resolves with the high res time stamp of the next animation frame
  */
-export const framus = <T = unknown>(
-  callback: FramusRequestCallback<T> | FramusOptions = {},
-  { signal }: FramusOptions = typeof callback === 'function' ? {} : callback
-): Promise<T | DOMHighResTimeStamp> => new Promise((resolve, reject) => {
-  const request = window.requestAnimationFrame(now => {
-    if (signal) {
-      signal.removeEventListener('abort', handleAbort)
+export function framus <T = unknown> (
+  callbackOrOptions: FramusRequestCallback<T> | FramusOptions = {},
+  { signal }: FramusOptions = typeof callbackOrOptions === 'function' ? {} : callbackOrOptions
+): Promise<T | DOMHighResTimeStamp> {
+  const callback = typeof callbackOrOptions === 'function' ? callbackOrOptions : null
+
+  return new Promise((resolve, reject) => {
+    const request = window.requestAnimationFrame(now => {
+      if (signal) {
+        signal.removeEventListener('abort', handleAbort)
+      }
+
+      resolve(callback ? callback(now) : now)
+    })
+
+    const handleAbort = () => {
+      window.cancelAnimationFrame(request)
+      reject(new DOMException('Cancelled animation frame request'))
     }
 
-    resolve(typeof callback === 'function' ? callback(now) : now)
+    if (signal) {
+      signal.addEventListener('abort', handleAbort)
+    }
   })
-
-  const handleAbort = () => {
-    window.cancelAnimationFrame(request)
-    reject(new DOMException('Cancelled animation frame request'))
-  }
-
-  if (signal) {
-    signal.addEventListener('abort', handleAbort)
-  }
-})
+}
